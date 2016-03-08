2d610f9ee045fc2c7600e96c288e2f6e5c87c940
Justin Isaf <code@justinisaf.com>
2016-03-08 15:40:10 -0500

    JAson is a jerk

--- app.js
+++ app.js
@@ -3,29 +3,11 @@ const app = express();
 const morgan = require('morgan');
 const chalk = require('chalk');
 const swig = require('swig');
+const bank = require('./tweetBank')
+const routes = require('./routes')
 
-//extra credit on 3. logging middleware
-// function getStatus(func){
-// 	var result = func()
-// 	return result.statusCode
-// }
-
-// replaced with morgan
-// function logger (req, res, next){
-// 	console.log(chalk.blue(req.method), chalk.yellow(req.url))
-// 	console.log(next())
-// 	// console.log(res.statusCode)
-
-// }
-
-// function specialLogger(req, res, next){
-// 	console.log("Special Area:", chalk.magenta(req.method), chalk.cyan(req.url))
-// 	next()
-// }
-
-// app.use(logger)
-// app.use('/special', specialLogger)
-// app.use(morgan)
+//do extra credit on 3. logging middleware
+//do extra credit on 7. static routing
 app.use(morgan('dev'));
 
 app.engine('html', swig.renderFile);
@@ -33,14 +15,9 @@ app.set('view engine','html');
 app.set('views',__dirname+'/views');
 swig.setDefaults({cache: false});
 
-app.get('/', function(req, res){
-	var people = [{name: 'Full'},{name: 'Stacker'},{name: 'Boy'}];
-	res.render('index', {title: 'Hall of Fame', people: people})
-});
+app.use('/', routes)
+app.use(express.static('public'))
 
-app.get('/news', function(req, res){
-	res.send(new Date());
-});
- 
-console.log("server listening");
+
+
 app.listen(3000);
